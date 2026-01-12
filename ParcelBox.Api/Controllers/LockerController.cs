91dac89a70c3aa3dc03bc4927391a49d22b01c20
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-01-12 20:53:07 +0100

    Adds locker endpoints and DI setup

---
+++ ParcelBox.Api/Controllers/LockerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParcelBox.Api.Abstraction;
+using ParcelBox.Api.Dtos.LockerBox;
+using ParcelBox.Api.Model;
+
+namespace ParcelBox.Api.Controllers;
+
+public class LockerController(IRepository<Locker> repository) 
+    : BaseController
+{
+    [HttpGet]
+    public IActionResult GetAllLockers()
+    {
+        var lockers = repository.GetAll();
+        
+        return Ok(lockers.Select(locker => new GetLockerResponse
+        {
+            Code = locker.Code,
+            Address = locker.Address,
+            City = locker.City,
+            LockerBoxes = locker.LockerBoxes
+        }));
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetLockerById(int id)
+    {
+        var locker = repository.GetById(id);
+
+        if (locker == null) return NotFound();
+
+        return Ok(new GetLockerResponse
+        {
+            Code = locker.Code,
+            Address = locker.Address,
+            City = locker.Code,
+            LockerBoxes = locker.LockerBoxes
+        });
+    }
+}
---
+++ ParcelBox.Api/Dtos/LockerBox/GetLockerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ParcelBox.Api.Dtos.LockerBox;
+
+public class GetLockerResponse
+{
+    public required string Code { get; set; }
+    public required string Address { get; set; }
+    public required string City { get; set; }
+
+    public Model.LockerBox[] LockerBoxes { get; set; } = [];
+}
