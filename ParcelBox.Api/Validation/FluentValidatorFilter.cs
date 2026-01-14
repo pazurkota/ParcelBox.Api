2619a7bed5cd30b2f5a49c344f3851661da3d411
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-01-14 17:47:22 +0100

    Adds FluentValidation-based request validation

---
+++ ParcelBox.Api/Validation/FluentValidatorFilter.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace ParcelBox.Api.Validation;
+
+public class FluentValidationFilter(IServiceProvider serviceProvider, ProblemDetailsFactory problemDetailsFactory)
+    : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var parameter in context.ActionDescriptor.Parameters)
+        {
+            if (!context.ActionArguments.TryGetValue(parameter.Name, out var argumentValue) ||
+                argumentValue == null) continue;
+            
+            var argumentType = argumentValue.GetType();
+            var validatorType = typeof(IValidator<>).MakeGenericType(argumentType);
+
+            if (serviceProvider.GetService(validatorType) is not IValidator validator) continue;
+            
+            // validate the argument
+            var validationResult = await validator.ValidateAsync(new ValidationContext<object>(argumentValue));
+            if (validationResult.IsValid) continue;
+            
+            // validationResult.AddToModelState(context.ModelState);
+            // this should work
+            foreach (var error in validationResult.Errors)
+            {
+                context.ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+            
+            var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(context.HttpContext, context.ModelState);
+            context.Result = new BadRequestObjectResult(problemDetails);
+
+            return;
+        }
+
+        await next();
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context) { }
+}
