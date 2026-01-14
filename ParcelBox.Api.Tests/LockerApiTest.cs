477302f0957cce512d3e6e01b16bb8ebe15b64db
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-01-14 17:42:52 +0100

    Adds locker box endpoint tests

--- ParcelBox.Api.Tests/LockerApiTest.cs
+++ ParcelBox.Api.Tests/LockerApiTest.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using ParcelBox.Api.Abstraction;
+using ParcelBox.Api.Dtos.Locker;
 using ParcelBox.Api.Model;
 
 namespace ParcelBox.Api.Tests;
@@ -11,39 +13,20 @@ public class LockerApiTest : IClassFixture<WebApplicationFactory<Program>>
     private const string BaseUrl = "api/lockers";
     
     private readonly HttpClient _client;
+    
     private readonly Locker _testLocker = new()
     {
         Id = 1,
-        Address = "1400 Defense Pentagon",
+        Code = "WAS-002",
+        Address = "1600 Pennsylvania Avenue NW",
         City = "Washington DC",
-        PostalCode = "20301 USA",
-        Code = "WAS-001",
-        LockerBoxes =
-        [
-            new LockerBox
-            {
-                Id = 1,
-                LockerSize = nameof(LockerSize.Small),
-                IsOccupied = false,
-                LockerId = 1
-            },
+        PostalCode = "20500 USA"
+    };
 
-            new LockerBox
-            {
-                Id = 2,
-                LockerSize = nameof(LockerSize.Medium),
-                IsOccupied = true,
-                LockerId = 1
-            },
-
-            new LockerBox
-            {
-                Id = 3,
-                LockerSize = nameof(LockerSize.Big),
-                IsOccupied = false,
-                LockerId = 1
-            }
-        ]
+    private readonly CreateLockerBoxDto[] _lockerBoxes = new[]
+    {
+        new CreateLockerBoxDto { LockerSize = "Small" },
+        new CreateLockerBoxDto {LockerSize = "Medium"}
     };
 
     public LockerApiTest(WebApplicationFactory<Program> factory)
@@ -51,7 +34,15 @@ public class LockerApiTest : IClassFixture<WebApplicationFactory<Program>>
         _client = factory.CreateClient();
 
         var repository = factory.Services.GetRequiredService<IRepository<Locker>>();
-        repository.Create(_testLocker);
+        repository.Create(new Locker()
+        {
+            Id = 1,
+            Address = "1400 Defense Pentagon",
+            City = "Washington DC",
+            PostalCode = "20301 USA",
+            Code = "WAS-001",
+            LockerBoxes = []
+        });
     }
 
     [Fact]
@@ -65,7 +56,7 @@ public class LockerApiTest : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public async Task GetLockerById_ReturnsOkResult()
     {
-        var response = await _client.GetAsync($"{BaseUrl}/{_testLocker.Id}");
+        var response = await _client.GetAsync($"{BaseUrl}/1");
 
         response.EnsureSuccessStatusCode();
     }
@@ -73,8 +64,24 @@ public class LockerApiTest : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public async Task GetLockerById_ReturnsNotFound()
     {
-        var response = await _client.GetAsync($"{BaseUrl}/{_testLocker.Id + 1}");
+        var response = await _client.GetAsync($"{BaseUrl}/0");
         
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreateLocker_ReturnsCreatedResult()
+    {
+        var response = await _client.PostAsJsonAsync($"{BaseUrl}/create", _testLocker);
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task CreateLockerBoxes_ReturnsCreatedResult()
+    {
+        var response = await _client.PatchAsJsonAsync($"{BaseUrl}/1/boxes", _lockerBoxes);
+
+        response.EnsureSuccessStatusCode();
     }
 }
