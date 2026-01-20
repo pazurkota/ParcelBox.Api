c274c1631771275d4699aa56351a6b8406e4d2c1
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-01-20 19:50:25 +0100

    Adds validation for locker size

---
+++ ParcelBox.Api/Validation/LockerBoxesValidation/CreateLockerBoxValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using ParcelBox.Api.Dtos.Locker;
+using ParcelBox.Api.Model;
+
+namespace ParcelBox.Api.Validation.LockerBoxesValidation;
+
+public class CreateLockerBoxValidator : AbstractValidator<CreateLockerBoxDto>
+{
+    public CreateLockerBoxValidator()
+    {
+        RuleFor(x => x.LockerSize)
+            .NotEmpty().
+            WithMessage("The locker size is required")
+            .IsInEnum().
+            WithMessage($"Locker size must be one of: {string.Join(", ", Enum.GetNames<Size>())}");
+    }
+}
