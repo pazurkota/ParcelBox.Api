a6b1b09f571a05ee81a77cb18c62c5c335d9e69b
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-01-24 19:49:43 +0100

    Adds initial locker seed data

---
+++ ParcelBox.Api/Database/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using ParcelBox.Api.Model;
+
+namespace ParcelBox.Api.Database;
+
+public static class SeedData
+{
+    public static void Seed(IServiceProvider provider)
+    {
+        var context = provider.GetRequiredService<AppDbContext>();
+
+        if (!context.Lockers.Any())
+        {
+            context.Lockers.AddRange(
+                new Locker
+                {
+                    Id = 1,
+                    Address = "1400 Defense Pentagon",
+                    City = "Washington DC",
+                    PostalCode = "20301 USA",
+                    Code = "WAS-001",
+                    LockerBoxes =
+                    [
+                        new LockerBox
+                        {
+                            Id = 1,
+                            LockerSize = Size.Small,
+                            IsOccupied = false,
+                            LockerId = 1
+                        },
+
+                        new LockerBox
+                        {
+                            Id = 2,
+                            LockerSize = Size.Medium,
+                            IsOccupied = true,
+                            LockerId = 1
+                        },
+
+                        new LockerBox
+                        {
+                            Id = 3,
+                            LockerSize = Size.Big,
+                            IsOccupied = false,
+                            LockerId = 1
+                        }
+                    ]
+                });
+            
+            context.SaveChanges();
+        }
+    }
+}
--- ParcelBox.Api/Program.cs
+++ ParcelBox.Api/Program.cs
@@ -36,6 +36,12 @@ builder.Services.AddControllers(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Seed(services);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
