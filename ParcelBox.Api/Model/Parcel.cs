49d51ce58b716f9328344ae200e7815029060862
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-02-05 23:37:07 +0100

    Add Parcel entity and migration

--- ParcelBox.Api/Model/Parcel.cs
+++ ParcelBox.Api/Model/Parcel.cs
@@ -3,8 +3,13 @@
 public class Parcel
 {
     public int Id { get; set; }
-    public required string PickupCode { get; set; }
     
-    public int InitialParcelId { get; set; }
-    public int TargetParcelId { get; set; }
+    public required string PickupCode { get; set; }
+    public Size ParcelSize { get; set; }
+    
+    public int InitialLockerId { get; set; }
+    public int TargetLockerId { get; set; }
+    
+    public int InitialLockerBoxId { get; set; }
+    public int TargetLockerBoxId { get; set; }
 }
