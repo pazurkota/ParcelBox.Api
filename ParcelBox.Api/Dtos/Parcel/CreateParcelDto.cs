210c1659c95572805103ff3610c119dfdaa71d10
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-02-05 23:39:12 +0100

    Add CreateParcelDto.cs

---
+++ ParcelBox.Api/Dtos/Parcel/CreateParcelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ParcelBox.Api.Dtos.Parcel;
+
+public class CreateParcelDto
+{
+    public required string PickupCode { get; set; }
+    public required string ParcelSize { get; set; }
+    
+    public int InitialLockerId { get; set; }
+    public int TargetLockerId { get; set; }
+}
