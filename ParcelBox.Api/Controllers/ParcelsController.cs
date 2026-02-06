d17e7cc75d5b9df9ef2f11698f13fb696bd6e65e
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-02-06 10:51:29 +0100

    Adds parcels listing and retrieval endpoints

---
+++ ParcelBox.Api/Controllers/ParcelsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParcelBox.Api.Database;
+using ParcelBox.Api.Dtos.Parcel;
+using ParcelBox.Api.Model;
+
+namespace ParcelBox.Api.Controllers;
+
+public class ParcelsController(AppDbContext dbContext) : BaseController
+{
+    public async Task<IActionResult> GetAllParcels([FromQuery] GetAllParcelsRequestDto requestDto)
+    {
+        int page = requestDto?.Page ?? 1;
+        int numberOfRecords = requestDto?.RecordsPerPage ?? 100;
+
+        var query = dbContext.Parcels
+            .Skip((page - 1) * numberOfRecords)
+            .Take(numberOfRecords);
+
+        if (!string.IsNullOrWhiteSpace(requestDto.Size))
+        {
+            if (!Enum.TryParse<Size>(requestDto.Size, ignoreCase: true, out var size))
+            {
+                return BadRequest($"Invalid locker size value: '{requestDto.Size}'.");
+            }
+
+            query = query.Where(x => x.ParcelSize == size);
+        }
+        
+        var parcels = await query.ToArrayAsync();
+
+        return Ok(parcels.Select(ParcelToGetParcelDto));
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetParcelById(int id)
+    {
+        var parcel = await dbContext.Parcels.SingleOrDefaultAsync(x => x.Id == id);
+        if (parcel is null) return NotFound();
+
+        var existingParcel = ParcelToGetParcelDto(parcel);
+        return Ok(existingParcel);
+    }
+
+    private static GetParcelDto ParcelToGetParcelDto(Parcel parcel)
+    {
+        return new GetParcelDto
+        {
+            PickupCode = parcel.PickupCode,
+            ParcelSize = parcel.ParcelSize.ToString(),
+            
+            InitialLockerId = parcel.InitialLockerId,
+            TargetLockerId = parcel.TargetLockerId,
+            
+            InitialLockerBoxId = parcel.InitialLockerBoxId,
+            TargetLockerBoxId = parcel.TargetLockerBoxId
+        };
+    }
+}
---
+++ ParcelBox.Api/Dtos/Parcel/GetParcelDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ParcelBox.Api.Dtos.Parcel;
+
+public class GetParcelDto
+{
+    public int Id { get; set; }
+    
+    public required string PickupCode { get; set; }
+    public required string ParcelSize { get; set; }
+    
+    public int InitialLockerId { get; set; }
+    public int TargetLockerId { get; set; }
+    
+    public int InitialLockerBoxId { get; set; }
+    public int TargetLockerBoxId { get; set; }
+}
