af5a25883642cad692ad84f84ccbe4f9f03ae15c
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-02-15 20:11:03 +0100

    Moved GetAllParcels tests

---
+++ ParcelBox.Api.Tests/ParcelsTests/GetAllParcelsApiTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace ParcelBox.Api.Tests.ParcelsTests;
+
+public class GetAllParcelsApiTest(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory> 
+{
+    private readonly HttpClient _client = factory.CreateClient();
+    
+    [Fact]
+    public async Task GetAllParcels_ReturnsOkResult()
+    {
+        var response = await _client.GetAsync(ApiLinks.ParcelsUrl);
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task GetAllParcels_WithQuery_ReturnsOkResult()
+    {
+        var response = await _client.GetAsync($"{ApiLinks.ParcelsUrl}?page=1&recordsPerPage=10");
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task GetAllParcels_WithSizeFilter_ReturnsOkResult()
+    {
+        var response = await _client.GetAsync($"{ApiLinks.ParcelsUrl}?size=Small");
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task GetAllParcels_WithInvalidSize_ReturnsBadRequest()
+    {
+        var response = await _client.GetAsync($"{ApiLinks.ParcelsUrl}?size=InvalidSize");
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
